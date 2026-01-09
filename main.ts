8ccf3f39cf36744af92cf76c8892469af6543a04
Ethan White <ethan.white1@stu.ocsb.ca>
2026-01-09 15:41:46 +0000

    Finished welcome page and began loop for asking for the questions and correct answers

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Ethan White
+ * @version 1.0.0
+ * @date 2026-1-7
+ * @fileoverview
+ */
+
+let questions: string[] = [];
+let answers: string[] = [];
+let numQ=Number(
+  prompt("Welcome to Trivia!\nHow many question would you like to quiz your friend on? Please input the number as a number not as a word")
+);
+while (numQ >= 1) {
+let question=prompt("Enter question :");
+let answer = prompt("Enter the correct answer for question :")
+numQ=numQ-1
+}
