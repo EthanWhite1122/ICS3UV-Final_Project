d84affe84b7ef27ad9dba349fce669c8cc113d28
Ethan White <ethan.white1@stu.ocsb.ca>
2026-01-14 02:42:30 +0000

    Fixed  some grammatical errors and made it more visually pleasing for coders and users

--- main.ts
+++ main.ts
@@ -10,17 +10,18 @@ let answers: string[] = [];
 let correct: number = 0;
 let index: number = 0;
 
-console.log("Welcome to Trivia!\nThis is a 2 player game where you make a mini quiz for your friend and they answer.")
-let numQ=Number(
-  prompt("How many question would you like to quiz your friend on? Please input the number as a number not as a word")
-);
+console.log("Welcome to Trivia!\nThis is a 2 player game where you make a mini quiz for your friend and they answer.\n Be sure to click enter after you find typing every input");
+  let numQ=Number(
+    prompt("How many questions would you like to quiz your friend on? Please input the number as a integer not as a word")
+  );
 
-while (isNaN(numQ) || numQ <= 0) {
-  numQ = Number(prompt("Please enter a valid number greater than 0."));
-}
+  while (isNaN(numQ) || numQ <= 0) {
+    numQ = Number(prompt("Please enter a valid number greater than 0."));
+  }
 
+let questionNum=1;
 while (index < numQ) {
-  let question = prompt("Enter question:");
+  let question = prompt("Enter question " + questionNum + ":");
   let answer = prompt("Enter the correct answer for the question:");
   if (question == null || answer == null || question == "" || answer == "") {
     console.log("Question and answer cannot be empty.");
@@ -32,11 +33,11 @@ while (index < numQ) {
 }
 
 let playIndex: number = 0;
-
+console.log(" \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 while (playIndex < numQ) {
-  let userAnswer=prompt(questions[playIndex]);
+  let userAnswer=prompt(questions[playIndex] + "?");
 
-  if (userAnswer === answers[playIndex]) {
+  if (userAnswer == answers[playIndex]) {
     console.log("Correct!");
     correct = correct + 1;
   } else {
@@ -49,4 +50,4 @@ while (playIndex < numQ) {
 }
 
 //Printing final results
-console.log("Quiz complete. You got a score of " + correct + " out of " + numQ + ".")
+console.log("Quiz complete. You got a score of " + correct + " out of " + numQ + ".");
